65c3b0669d5716c93f29196efb8a4ebf05bbe8f8
Nicolai800 <happy455420@gmail.com>
2024-11-28 11:37:20 +0100

    PongGame js paddle 1 and 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // JS from the DOM
 const startText = document.getElementById("startText");
 const paddle1 = document.getElementById("paddle1");
+const paddle2 = document.getElementById("paddle2");
 
 // Game Variables
 let gameRunning = false;
@@ -8,10 +9,14 @@ let keyPressed = {};
 let paddle1Speed = 0;
 let paddle2Speed = 0;
 let paddle1Y = 150;
+let paddle2Y = 150;
 
 // Game const
 const paddleAcceleration = 1;
+const paddleDeceleration = 1;
 const maxPaddleSpeed = 5;
+const gameHeigth = 400;
+const gameWidth = 600;
 
 document.addEventListener("keydown", startGame);
 document.addEventListener("keydown", handleKeyDown);
@@ -28,13 +33,13 @@ function startGame() {
 function gameLoop() {
   if (gameRunning) {
     updatePaddle1();
+    updatePaddle2();
     setTimeout(gameLoop, 8);
   }
 }
 
 function handleKeyDown(e) {
   keyPressed[e.key] = true;
-  console.log(keyPressed);
 }
 function handleKeyUp(e) {
   keyPressed[e.key] = false;
@@ -43,8 +48,43 @@ function handleKeyUp(e) {
 function updatePaddle1() {
   if (keyPressed["w"]) {
     paddle1Speed = Math.max(paddle1Speed - paddleAcceleration, -maxPaddleSpeed);
+  } else if (keyPressed["s"]) {
+    paddle1Speed = Math.min(paddle1Speed + paddleAcceleration, maxPaddleSpeed);
+  } else if (paddle1Speed > 0) {
+    paddle1Speed = Math.max(paddle1Speed - paddleDeceleration, 0);
+  } else if (paddle1Speed < 0) {
+    paddle1Speed = Math.min(paddle1Speed + paddleDeceleration, 0);
   }
 
   paddle1Y += paddle1Speed;
+
+  if (paddle1Y < 0) {
+    paddle1Y = 0;
+  }
+  if (paddle1Y > gameHeigth - paddle1.clientHeight) {
+    paddle1Y = gameHeigth - paddle1.clientHeight;
+  }
   paddle1.style.top = paddle1Y + "px";
+}
+
+function updatePaddle2() {
+  if (keyPressed["ArrowUp"]) {
+    paddle2Speed = Math.max(paddle2Speed - paddleAcceleration, -maxPaddleSpeed);
+  } else if (keyPressed["ArrowDown"]) {
+    paddle2Speed = Math.min(paddle2Speed + paddleAcceleration, maxPaddleSpeed);
+  } else if (paddle2Speed > 0) {
+    paddle2Speed = Math.max(paddle2Speed - paddleDeceleration, 0);
+  } else if (paddle2Speed < 0) {
+    paddle2Speed = Math.min(paddle2Speed + paddleDeceleration, 0);
+  }
+
+  paddle2Y += paddle2Speed;
+
+  if (paddle2Y < 0) {
+    paddle2Y = 0;
+  }
+  if (paddle2Y > gameHeigth - paddle2.clientHeight) {
+    paddle2Y = gameHeigth - paddle2.clientHeight;
+  }
+  paddle2.style.top = paddle2Y + "px";
 }
